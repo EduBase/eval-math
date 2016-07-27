dc1824f131a745caacafb1cbcf89602bf0d4e65d
Hives Áron <icebird93@gmail.com>
2016-07-27 14:56:09 +0200

    Modulo fix
    Modulo custom function bug fix

--- src/EvalMath.php
+++ src/EvalMath.php
@@ -385,7 +385,7 @@ function csc($in){ return 1/sin($in); }
 function sec($in){ return 1/cos($in); }
 function sgn($in){ return ($in>0)?1:(($in<0)?-1:0); }
 
-function modulo($n,$i){ return $n+$i; }
+function modulo($n,$i){ return $n%$i; }
 
 function isprime($n)
 {
